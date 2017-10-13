afd91dd263cce10045710091da1d8e9930e61bb8
Raul Jordan <raulitojordan@gmail.com>
2017-10-13 15:15:36 -0600

    toy blockchai

--- public/app.js
+++ public/app.js
@@ -22,7 +22,21 @@ var App = {
   },
   addListeners() {
     var btn = document.getElementById('start-mining');
+    var gif1 = document.getElementById('gif-1');
+    var gif2 = document.getElementById('gif-2');
+    var gif3 = document.getElementById('gif-3');
+    var gif4 = document.getElementById('gif-4');
+    gif1.addEventListener('click', this.copy);
+    gif2.addEventListener('click', this.copy);
+    gif3.addEventListener('click', this.copy);
+    gif4.addEventListener('click', this.copy);
+
+
     btn.addEventListener('click', this.mine);
+  },
+  copy(event) {
+    event.preventDefault();
+    console.log(event.target.href);
   },
   mine(event) {
     event.preventDefault();
@@ -88,6 +102,29 @@ var App = {
       });
   }
 };
+
+function copyToClipboard(text) {
+  if (window.clipboardData && window.clipboardData.setData) {
+    // IE specific code path to prevent textarea being shown while dialog is visible.
+    return clipboardData.setData("Text", text); 
+
+  } 
+  else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+    var textarea = document.createElement("textarea");
+    textarea.textContent = text;
+    textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in MS Edge.
+    document.body.appendChild(textarea);
+    textarea.select();
+    try {
+      return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+    } catch (ex) {
+      console.warn("Copy to clipboard failed.", ex);
+      return false;
+    } finally {
+      document.body.removeChild(textarea);
+    }
+  }
+}
 
 window.onload = function() {
   App.init();
